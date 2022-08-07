b309666228773189d7420fc3a6bd2ca97aca38ec
Coyocon <leroz_@hotmail.com>
2022-08-07 09:04:07 -0500

    Localizar

--- nosotros/enviar.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $correo_destino = 'cesarcoyoc@gmail.com';
-    $asunto = 'prueba';
-    $mensaje = $_POST['msg']
-	mail($correo_destino, $asunto, utf8_decode($mensaje);
-    header("Location:Index.html");
-?>
